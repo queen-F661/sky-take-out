1ac1c2fff78d1c932e096c7a3c9f921b9c6d1688
张涵予 <3168543442@qq.com>
2026-09-21 16:48:02 +0800

    商家订单详情（复用 details）

--- sky-server/src/main/java/com/sky/controller/admin/OrderController.java
+++ sky-server/src/main/java/com/sky/controller/admin/OrderController.java
@@ -10,6 +10,7 @@ import com.sky.service.OrderService;
 import com.sky.vo.OrderPaymentVO;
 import com.sky.vo.OrderStatisticsVO;
 import com.sky.vo.OrderSubmitVO;
+import com.sky.vo.OrderVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.annotation.Order;
@@ -47,5 +48,16 @@ public class OrderController {
         OrderStatisticsVO orderStatisticsVO = orderService.statistics();
 
         return Result.success(orderStatisticsVO);
+    }
+
+    /**
+     * 商家订单详情（复用 details）
+     * */
+    @GetMapping("/details/{id}")
+    public Result<OrderVO> details(@PathVariable Long id){
+
+        OrderVO orderVO = orderService.detail(id);
+
+        return Result.success(orderVO);
     }
 }
